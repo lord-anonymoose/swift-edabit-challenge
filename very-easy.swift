80a23648edb2ff836394f19781629c066c17adcd
Philipp <64854661+lord-anonymoose@users.noreply.github.com>
2020-09-30 13:14:25 +0300

    Create very-easy.swift

---
+++ very-easy.swift
@@ -0,0 +1,9 @@
+// Football points
+func footballPoints(_ wins: Int, _ draws: Int, _ losses: Int) -> Int {
+	return wins*3 + draws*1
+}
+
+// Return something to me!
+func giveMeSomething(_ a: String) -> String {
+	return "something " + a
+}
